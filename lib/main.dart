6ebed5d4bf5965fd583eb9ff32ffae3a738da593
insanephin <insanephin@gmail.com>
2022-04-20 08:44:50 +0900

    chapter 1 - fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() {
-    runApp(const MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-    @override
-    Widget build(BuildContext context) {
-        return MaterialApp(
-            title: 'Welcome to Flutter',
-            home: Scaffold(
-                appBar: AppBar(
-                    title: Text('Welcome to Flutter'),
-                ),
-                body: Center(
-                    child: Text('Hello World'),
-                ),
-            ),
-        );
-    }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Welcome to Flutter'),
+        ),
+        body: Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
 }
