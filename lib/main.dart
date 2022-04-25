bfde0938a67314767b56d6e3e6ef30938005c37d
InsanePhin <insanephin@gmail.com>
2022-04-25 21:24:27 +0900

    fix syntax

--- lib/main.dart
+++ lib/main.dart
@@ -66,7 +66,7 @@ class Home extends StatelessWidget {
                         ),
                     ],
                 ),
-            )
+            ),
         
             extendBodyBehindAppBar: true,
 
