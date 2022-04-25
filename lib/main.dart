a513f0972ed5a0f6ecc2117f578e8c5d9247adcf
InsanePhin <insanephin@gmail.com>
2022-04-25 21:19:22 +0900

    add drawer

--- lib/main.dart
+++ lib/main.dart
@@ -4,41 +4,78 @@ void main() {
   runApp(Portfolio());
 }
 
-class Portfolio extends StatefulWidget {
-  @override
-  _PortfoliiState createState() => _PortfoliiState();
+class Portfolio extends StatelessWidget {
+    @override
+    Widget build(BuildContext context) {
+        return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: "InsanePhin",
+            
+            theme: ThemeData(
+                fontFamily: 'Sunflower',
+                brightness: Brightness.light,
+            ),
+            darkTheme: ThemeData(
+                brightness: Brightness.dark
+            ),
+            
+            home: Home(),
+        );
+    }
 }
 
-class _PortfoliiState extends State<Portfolio> {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "InsanePhin",
-      theme: ThemeData(
-        fontFamily: 'Sunflower',
-        brightness: Brightness.light,
-      ),
-      darkTheme: ThemeData(
-        brightness: Brightness.dark
-      ),
-      
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Hi! I'm InsanePhin"),
-          leading: Icon(Icons.menu_outlined),
-          centerTitle: true,
-          backgroundColor: Colors.transparent,
-          elevation: 0,
-        ),
+class Home extends StatelessWidget {
+    @override
+    Widget build(BuildContext context) {
+        return Scaffold(
+            appBar: AppBar(
+                title: Text("Hi! I'm InsanePhin"),
+                leading: Icon(Icons.menu_outlined),
+                centerTitle: true,
+                backgroundColor: Colors.transparent,
+                elevation: 0,
+            ),
+            
+            drawer: Drawer(
+                child: ListView(
+                    padding: EdgeInsets.zero,
+                    children: [
+                        DrawerHeader(
+                            child: Text('Menu'),
+                            decoration: BoxDecoration(
+                                
+                            ),
+                        ),
+                        ListTile(
+                            title: Text('소개'),
+                            onTap: () {
+                                Navigator.pop(context);
+                            },
+                        ),
+                        ListTile(
+                            title: Text('프로젝트'),
+                            onTap: () {
+                                Navigator.pop(context);
+                            },
+                        ),
+                        ListTile(
+                            title: Text('감사한분들'),
+                            onTap: () {
+                                Navigator.pop(context);
+                            },
+                        ),
+                    ],
+                ),
+            )
         
-        extendBodyBehindAppBar: true,
+            extendBodyBehindAppBar: true,
 
-        body: Center(
-          child: Text("Coming Soon", style: TextStyle(fontSize: 30),
-          ),
-        ),
-      ),
-    );
-  }
+            body: Center(
+                child: Text(
+                    "Coming Soon", 
+                    style: TextStyle(fontSize: 30),
+                ),
+            ),
+        )
+    }
 }
